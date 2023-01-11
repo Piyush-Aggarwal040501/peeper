34530eab9f338f69e45416fb6f7877c87dabd91c
Piyush Aggarwal <aggarwalpiyush85@gmail.com>
2023-01-11 13:32:55 +0530

    navbar and footer

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ import About from './components/About';
 import Ratings from './components/Ratings';
 import Career from './components/Career';
 import HomeCard from './minors/HomeCard';
+import { Footer } from './components/Footer';
+import NavScrollExample from './components/NavScrollExample';
+
 
 function App() {
 	return (
-		<div id='mainBox'>
-			<Home />
-			<About />
-			<Ratings />
-			<Career />
-			<HomeCard heading={"Focus on the analytics that matters"} top={false} />
+		<div>
+			<NavScrollExample />
+			<div id='mainBox'>
+				<Home />
+				<About />
+				<Ratings />
+				<Career />
+				<HomeCard heading={"Focus on the analytics that matters"} top={false} />
+			</div>
+			<Footer />
 		</div>
 	);
 }
